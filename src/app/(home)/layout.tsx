5e1331f63313773ac921381d092510d56d1d217d
jinhok96 <jinhok96a@gmail.com>
2025-12-02 15:49:45 +0900

    [25.12.02] 홈페이지 레이아웃 분리

---
+++ src/app/(home)/layout.tsx
@@ -0,0 +1,7 @@
+import type { PropsWithChildren } from 'react';
+
+type Props = PropsWithChildren;
+
+export default function HomeLayout({ children }: Props) {
+  return <div className="container flex-col-center size-full flex-1 px-layout">{children}</div>;
+}
---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from 'next';
+
+import Link from 'next/link';
+
+import { ROUTER } from '@/core/config';
+import { generatePageMetadata } from '@/core/utils';
+
+export const metadata: Metadata = generatePageMetadata({ routerName: 'home' });
+
+export default function HomePage() {
+  return (
+    <div className="size-full">
+      <h1 className="font-title-44">
+        안녕하세요,
+        <br />
+        프론트엔드 개발자
+        <br />
+        <span className="text-blue-6">강진호</span>입니다.
+      </h1>
+      <p>소개, 이력서 다운, 핵심 역량, 기술 스택, 경력 사항, 프로젝트 상세, 블로그, 교육, 연락처</p>
+      <div>
+        <Link href={ROUTER.projects}>Projects</Link>
+      </div>
+      <div>
+        <Link href={ROUTER.blog}>Blog</Link>
+      </div>
+      <div>
+        <Link href={ROUTER.libraries}>Libraries</Link>
+      </div>
+    </div>
+  );
+}

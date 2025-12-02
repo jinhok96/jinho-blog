5de1958342a09c465847f3cf5e8b32f0da04e7fb
jinhok96 <jinhok96a@gmail.com>
2025-12-02 12:14:08 +0900

    [25.12.02] 필요없는 스토어 버튼 삭제

--- src/core/ui/button/index.ts
+++ src/core/ui/button/index.ts
@@ -1,5 +1,4 @@
 export { Button } from './Button';
 export { LinkAButton } from './LinkAButton';
 export { LinkButton } from './LinkButton';
-export { StoreLinkButton } from './StoreLinkButton';
 export type { ButtonProps } from './types';

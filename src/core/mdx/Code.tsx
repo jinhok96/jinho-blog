b9b8fd6d8222fe76b046d5a50fc571a9b763e588
jinhok96 <jinhok96a@gmail.com>
2025-12-02 12:09:12 +0900

    [25.12.02] 테마별 코드 스타일 분리

--- src/core/mdx/Code.tsx
+++ src/core/mdx/Code.tsx
@@ -1,7 +1,7 @@
 import type { ComponentProps } from 'react';
 
 import { Prism } from 'react-syntax-highlighter';
-import { atomDark as style } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { oneDark as darkStyle, oneLight as lightStyle } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import localFont from 'next/font/local';
 
 import { cn } from '@/core/utils';
@@ -15,15 +15,39 @@ type Props = ComponentProps<typeof Prism>;
 
 export default function Code({ children, language = 'typescript', className, PreTag, ...props }: Props) {
   return (
-    <Prism
-      {...props}
-      className={cn('m-0! inline-flex bg-transparent! p-0! text-[0.875rem] tracking-normal! whitespace-pre', className)}
-      language={language}
-      PreTag={PreTag}
-      codeTagProps={{ className: monaco.className }}
-      style={style}
-    >
-      {children}
-    </Prism>
+    <>
+      <Prism
+        {...props}
+        className={cn(
+          `
+            m-0! inline-flex bg-transparent! p-0! text-[0.875rem] tracking-normal! whitespace-pre
+            light:hidden
+          `,
+          className,
+        )}
+        language={language}
+        PreTag={PreTag}
+        codeTagProps={{ className: monaco.className }}
+        style={darkStyle}
+      >
+        {children}
+      </Prism>
+      <Prism
+        {...props}
+        className={cn(
+          `
+            m-0! inline-flex bg-transparent! p-0! text-[0.875rem] tracking-normal! whitespace-pre
+            dark:hidden
+          `,
+          className,
+        )}
+        language={language}
+        PreTag={PreTag}
+        codeTagProps={{ className: monaco.className }}
+        style={lightStyle}
+      >
+        {children}
+      </Prism>
+    </>
   );
 }

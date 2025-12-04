a782976fd47340cc06fbe594875ae721111f7e76
jinhok96 <jinhok96a@gmail.com>
2025-12-04 11:17:53 +0900

    [25.12.04] Next.js 버전 업데이트에 따라 몇 가지 설정 변경

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,8 @@ const nextConfig: NextConfig = {
     globalNotFound: true,
   },
 
+  cacheComponents: true,
+
   // turbopack 설정
   turbopack: {
     rules: {
@@ -15,36 +17,36 @@ const nextConfig: NextConfig = {
     },
   },
 
-  // webpack 설정
-  webpack: config => {
-    // @ts-expect-error rule 타입 에러 무시
-    const fileLoaderRule = config.module.rules.find(rule => rule.test?.test?.('.svg'));
+  // webpack 설정 -> 빌드에서도 turbopack을 사용하므로 필요없음
+  // webpack: config => {
+  //   // @ts-expect-error rule 타입 에러 무시
+  //   const fileLoaderRule = config.module.rules.find(rule => rule.test?.test?.('.svg'));
 
-    config.module.rules.push(
-      {
-        ...fileLoaderRule,
-        test: /\.svg$/i,
-        resourceQuery: /url/,
-      },
-      {
-        test: /\.svg$/i,
-        issuer: fileLoaderRule.issuer,
-        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
-        use: [
-          {
-            loader: '@svgr/webpack',
-            options: {
-              typescript: true,
-              ext: 'tsx',
-            },
-          },
-        ],
-      },
-    );
+  //   config.module.rules.push(
+  //     {
+  //       ...fileLoaderRule,
+  //       test: /\.svg$/i,
+  //       resourceQuery: /url/,
+  //     },
+  //     {
+  //       test: /\.svg$/i,
+  //       issuer: fileLoaderRule.issuer,
+  //       resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
+  //       use: [
+  //         {
+  //           loader: '@svgr/webpack',
+  //           options: {
+  //             typescript: true,
+  //             ext: 'tsx',
+  //           },
+  //         },
+  //       ],
+  //     },
+  //   );
 
-    fileLoaderRule.exclude = /\.svg$/i;
-    return config;
-  },
+  //   fileLoaderRule.exclude = /\.svg$/i;
+  //   return config;
+  // },
 };
 
 export default nextConfig;

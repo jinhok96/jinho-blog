305ab804af3d4a34f3af202e4d0c64ad0440086b
jinhok96 <jinhok96a@gmail.com>
2025-12-05 09:24:15 +0900

    [25.12.05] 브라우저에서 Node.js 네이티브 모듈을 로드하지 않도록 설정

--- next.config.ts
+++ next.config.ts
@@ -9,44 +9,20 @@ const nextConfig: NextConfig = {
 
   // turbopack 설정
   turbopack: {
+    resolveAlias: {
+      // 브라우저에서 Node.js 네이티브 모듈을 참조하지 않고 빈 모듈을 로드하도록 설정
+      fs: {
+        browser: './empty.ts',
+      },
+    },
     rules: {
+      // svgr 설정
       '*.svg': {
         loaders: ['@svgr/webpack'],
         as: '*.js',
       },
     },
   },
-
-  // webpack 설정 -> 빌드에서도 turbopack을 사용하므로 필요없음
-  // webpack: config => {
-  //   // @ts-expect-error rule 타입 에러 무시
-  //   const fileLoaderRule = config.module.rules.find(rule => rule.test?.test?.('.svg'));
-
-  //   config.module.rules.push(
-  //     {
-  //       ...fileLoaderRule,
-  //       test: /\.svg$/i,
-  //       resourceQuery: /url/,
-  //     },
-  //     {
-  //       test: /\.svg$/i,
-  //       issuer: fileLoaderRule.issuer,
-  //       resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
-  //       use: [
-  //         {
-  //           loader: '@svgr/webpack',
-  //           options: {
-  //             typescript: true,
-  //             ext: 'tsx',
-  //           },
-  //         },
-  //       ],
-  //     },
-  //   );
-
-  //   fileLoaderRule.exclude = /\.svg$/i;
-  //   return config;
-  // },
 };
 
 export default nextConfig;

01c8e254fb13c8430e1ebe2ba05b3c849bf95b12
jinhok96 <jinhok96a@gmail.com>
2025-12-05 16:40:59 +0900

    [25.12.05] SelectCategory 스타일 수정

--- src/features/selectCategory/ui/SelectCategory.tsx
+++ src/features/selectCategory/ui/SelectCategory.tsx
@@ -2,16 +2,28 @@
 
 import { Dropdown } from '@/core/ui';
 
-type Props = { prop?: string };
+type Props<T extends string> = { categories: T[] };
 
-export function SelectCategory({}: Props) {
+export function SelectCategory<T extends string>({ categories }: Props<T>) {
   return (
     <Dropdown>
       <Dropdown.Trigger>Category</Dropdown.Trigger>
-      <Dropdown.Container>
-        <Dropdown.Item>옵션1</Dropdown.Item>
-        <Dropdown.Item>옵션2</Dropdown.Item>
-        <Dropdown.Item>옵션3</Dropdown.Item>
+      <Dropdown.Container
+        className={`
+          rounded-xl border border-foreground-2 bg-background-5 py-2 shadow-lg shadow-black/15 backdrop-blur-md
+          dark:border-foreground-3 dark:bg-foreground-2
+        `}
+      >
+        {categories.map(category => (
+          <Dropdown.Item
+            key={category}
+            className="rounded-none text-left"
+            size="md"
+            color="background"
+          >
+            {category}
+          </Dropdown.Item>
+        ))}
       </Dropdown.Container>
     </Dropdown>
   );

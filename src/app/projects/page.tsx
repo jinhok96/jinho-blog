5880297daf13abb59beb5f5a85956d9632eb622e
jinhok96 <jinhok96a@gmail.com>
2025-12-08 11:42:36 +0900

    [25.12.08] 특정 라벨에 SafeHTML과 nbsp 적용

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,12 @@
+import type { ProjectCategory } from '@/core/types';
 import type { Metadata } from 'next';
 
-import { AsyncBoundary } from '@/core/ui';
-import { generatePageMetadata } from '@/core/utils';
+import { PROJECT_CATEGORY_MAP } from '@/core/map';
+import { AsyncBoundary, SafeHTML, type SelectOption } from '@/core/ui';
+import { generatePageMetadata, nbsp } from '@/core/utils';
+
+import { SelectCategory } from '@/features/selectCategory';
+import { SelectSort } from '@/features/selectSort';
 
 import { ProjectsContentSection } from '@/views/projects';
 
@@ -10,6 +15,11 @@ export const metadata: Metadata = generatePageMetadata({
   title: '프로젝트',
   description: '프로젝트 목록',
 });
+
+const CATEGORY_OPTIONS: SelectOption<ProjectCategory>[] = [
+  { key: 'tripmoney', label: PROJECT_CATEGORY_MAP.tripmoney },
+  { key: 'personal', label: <SafeHTML html={nbsp(PROJECT_CATEGORY_MAP.personal)} /> },
+];
 
 type Props = {
   searchParams: Promise<Record<string, string | string[] | undefined>>;
@@ -20,10 +30,19 @@ export default async function ProjectsListPage({ searchParams }: Props) {
     <div className="flex-col-start size-full gap-6">
       <h1 className="font-title-36">프로젝트</h1>
 
-      <div className="flex-row-center w-full justify-between">
-        <div>카테고리, 화면을 모달로 띄우기 (Parallel Route)</div>
-        <div>필터</div>
+      <div className="z-10 flex-row-center w-full justify-between">
+        <AsyncBoundary>
+          <SelectCategory
+            options={CATEGORY_OPTIONS}
+            position="bottomLeft"
+          />
+        </AsyncBoundary>
+        <AsyncBoundary>
+          <SelectSort position="bottomRight" />
+        </AsyncBoundary>
       </div>
+
+      <div>화면을 모달로 띄우기 (Parallel Route)</div>
 
       <AsyncBoundary>
         <ProjectsContentSection searchParams={searchParams} />

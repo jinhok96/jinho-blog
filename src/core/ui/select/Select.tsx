80748cf7ec306cd76b71fabe04c967055897cf1b
jinhok96 <jinhok96a@gmail.com>
2025-12-08 11:16:28 +0900

    [25.12.08] Select.Container 스타일 수정

--- src/core/ui/select/Select.tsx
+++ src/core/ui/select/Select.tsx
@@ -24,7 +24,7 @@ function Container({ className, ...props }: ContainerProps) {
     <Dropdown.Container
       className={cn(
         `
-          overflow-y-auto rounded-xl border border-foreground-2 bg-background-5 py-2 shadow-lg shadow-black/15
+          overflow-y-auto rounded-xl border border-foreground-2 bg-background-4 py-2 shadow-lg shadow-black/15
           backdrop-blur-lg
           dark:border-foreground-3 dark:bg-foreground-2
         `,

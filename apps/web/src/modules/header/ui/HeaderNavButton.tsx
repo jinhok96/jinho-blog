dd915136ca7c4b94e183969f922527b8e3abae6a
jinhok96 <jinhok96a@gmail.com>
2025-12-17 13:41:21 +0900

    [25.12.17] HeaderNavButton 타입 오류 수정, 코드 정리

--- apps/web/src/modules/header/ui/HeaderNavButton.tsx
+++ apps/web/src/modules/header/ui/HeaderNavButton.tsx
@@ -8,10 +8,11 @@ import { routes } from '@jinho-blog/nextjs-routes';
 import { LinkButton } from '@/core/ui';
 import { cn } from '@/core/utils';
 
-type Props = Omit<ComponentProps<typeof LinkButton>, 'color' | 'size' | 'className'>;
+type Props = Omit<ComponentProps<typeof LinkButton>, 'color' | 'size' | 'className' | 'href'> & { href: string };
 
 export function HeaderNavButton({ href, children, ...props }: Props) {
   const pathname = usePathname();
+
   const isActive =
     href === routes({ pathname: '/' }) ? pathname === routes({ pathname: '/' }) : pathname.startsWith(href);
 

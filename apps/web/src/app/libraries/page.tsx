54a387cdcab934e27482d978a9a151ff0555b155
jinhok96 <jinhok96a@gmail.com>
2025-12-18 16:33:19 +0900

    [25.12.18] 필요없는 기본값 빈 배열 제거

--- apps/web/src/app/libraries/page.tsx
+++ apps/web/src/app/libraries/page.tsx
@@ -1,16 +1,19 @@
+import type { LibraryCategory } from '@jinho-blog/shared';
 import type { Metadata } from 'next';
 
-import { routes, type SearchParams } from '@jinho-blog/nextjs-routes';
+import { routes } from '@jinho-blog/nextjs-routes';
 
-import { generatePageMetadata, parseSearchParams } from '@/core/utils';
+import { LIBRARY_CATEGORY_MAP } from '@/core/map';
+import { generatePageMetadata } from '@/core/utils';
 
-import { createLibrariesService, type GetLibraries } from '@/entities/libraries';
-
-import { Pagination } from '@/features/pagination';
+import { createLibrariesService } from '@/entities/libraries';
 
 import { LibrariesContentSection } from '@/views/libraries';
 
 const librariesService = createLibrariesService();
+
+const LIMIT = 6;
+const LIBRARY_CATEGORIES: LibraryCategory[] = ['react', 'nextjs', 'swr', 'motion', 'zustand'];
 
 export const metadata: Metadata = generatePageMetadata({
   path: routes({ pathname: '/libraries' }),
@@ -18,33 +21,40 @@ export const metadata: Metadata = generatePageMetadata({
   description: '라이브러리 목록',
 });
 
-type Props = {
-  searchParams: Promise<SearchParams<Record<keyof GetLibraries['search'], string | string[] | undefined>>>;
-};
+export default async function LibrariesListPage() {
+  const { items } = await librariesService.getLibraries();
 
-export default async function LibrariesListPage({ searchParams }: Props) {
-  const { category, sort, tech, page, count, search } = await searchParams;
+  // 카테고리별 그룹화
+  const librariesByCategory = items.reduce(
+    (result, item) => {
+      if (!LIBRARY_CATEGORIES.includes(item.category)) return result;
 
-  const getLibrariesParams: GetLibraries['search'] = {
-    category: parseSearchParams.category(category),
-    sort: parseSearchParams.sort(sort),
-    tech: parseSearchParams.tech(tech),
-    page: parseSearchParams.page(page)?.toString(),
-    count: parseSearchParams.count(count)?.toString(),
-    search: parseSearchParams.search(search)?.join(','),
-  };
+      if (!result[item.category]) result[item.category] = [];
+      if (result[item.category].length < LIMIT) result[item.category].push(item);
 
-  const { items, pagination } = await librariesService.getLibraries(getLibrariesParams);
+      return result;
+    },
+    {} as Record<LibraryCategory, typeof items>,
+  );
 
   return (
     <div className="flex-col-start size-full gap-6">
       <h1 className="font-title-36">라이브러리</h1>
 
-      <p>사이드바 목록, 모바일에서 드로어</p>
+      <p>라이브러리 페이지 설명입니다. 이 페이지의 목적을 설명합니다. 지속적으로 추가할 예정입니다.</p>
 
-      <LibrariesContentSection libraries={items} />
+      <div className="flex-col-start gap-14 pt-8">
+        {LIBRARY_CATEGORIES.map(category => (
+          <section
+            key={category}
+            className="w-full"
+          >
+            <p className="mb-5 font-subtitle-24">{LIBRARY_CATEGORY_MAP[category]}</p>
 
-      <Pagination pagination={pagination} />
+            <LibrariesContentSection libraries={librariesByCategory[category]} />
+          </section>
+        ))}
+      </div>
     </div>
   );
 }

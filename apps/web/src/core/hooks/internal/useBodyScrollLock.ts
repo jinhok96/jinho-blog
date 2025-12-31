d8bdce7a96326afa51ed6a22278a9c0130e7be09
jinhok96 <jinhok96a@gmail.com>
2025-12-31 13:50:50 +0900

    [25.12.31] useBodyScrollLock이 레이아웃 시프트를 발생시키지 않도록 개선

--- apps/web/src/core/hooks/internal/useBodyScrollLock.ts
+++ apps/web/src/core/hooks/internal/useBodyScrollLock.ts
@@ -1,13 +1,34 @@
 import { useLayoutEffect } from 'react';
 
+/**
+ * 스크롤을 방지하고 현재 위치 반환
+ */
+const preventScroll = (): number => {
+  const currentScrollY = window.scrollY;
+
+  document.body.style.position = 'fixed';
+  document.body.style.top = `-${currentScrollY}px`; // 현재 스크롤 위치
+  document.body.style.overflowY = 'scroll';
+
+  return currentScrollY;
+};
+
+/**
+ * 스크롤을 허용하고 이전 스크롤 위치 복구
+ */
+const allowScroll = (prevScrollY: number) => {
+  document.body.style.position = '';
+  document.body.style.top = '';
+  document.body.style.overflowY = '';
+
+  window.scrollTo({ top: prevScrollY, behavior: 'instant' });
+};
+
 export function useBodyScrollLock(isLocked: boolean): void {
   useLayoutEffect(() => {
-    if (isLocked) {
-      document.body.style.overflow = 'hidden';
-    }
+    if (!isLocked) return;
 
-    return () => {
-      document.body.style.overflow = '';
-    };
+    const prevScrollY = preventScroll();
+    return () => allowScroll(prevScrollY);
   }, [isLocked]);
 }

ec5abd908aa55bb47717fbdde1d00ac7b0e03dbd
jinhok96 <jinhok96a@gmail.com>
2026-01-05 16:53:57 +0900

    [25.1.5] HomeSection 프롭 확장

--- apps/web/src/views/home/ui/HomeSection.tsx
+++ apps/web/src/views/home/ui/HomeSection.tsx
@@ -1,15 +1,17 @@
-import type { PropsWithChildren } from 'react';
+import type { DetailedHTMLProps, HTMLAttributes } from 'react';
 
 import { cn } from '@/core/utils';
 
-type HomeSectionProps = PropsWithChildren<{
-  className?: string;
+type HomeSectionProps = DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement> & {
   innerClassName?: string;
-}>;
+};
 
-export function HomeSection({ children, className, innerClassName }: HomeSectionProps) {
+export function HomeSection({ children, className, innerClassName, ...props }: HomeSectionProps) {
   return (
-    <section className={cn('w-full px-layout', className)}>
+    <section
+      className={cn('w-full px-layout', className)}
+      {...props}
+    >
       <div className={cn('container mx-auto my-layout flex-col-center size-full gap-8', innerClassName)}>
         {children}
       </div>

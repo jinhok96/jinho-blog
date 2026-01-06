628d59ba2d56a443916687f20ad04aa8dfd360a8
jinhok96 <jinhok96a@gmail.com>
2026-01-06 13:47:08 +0900

    [25.1.6] useBodyScrollLock의 내부 함수 및 변수 이름 변경

--- apps/web/src/core/hooks/internal/useBodyScrollLock.ts
+++ apps/web/src/core/hooks/internal/useBodyScrollLock.ts
@@ -3,25 +3,25 @@ import { useLayoutEffect } from 'react';
 /**
  * 스크롤을 방지하고 현재 위치 반환
  */
-const preventScroll = (): number => {
-  const currentScrollY = window.scrollY;
+const lock = (): number => {
+  const scrollY = window.scrollY;
 
   document.body.style.position = 'fixed';
-  document.body.style.top = `-${currentScrollY}px`; // 현재 스크롤 위치
+  document.body.style.top = `-${scrollY}px`; // 현재 스크롤 위치
   document.body.style.overflowY = 'scroll';
 
-  return currentScrollY;
+  return scrollY;
 };
 
 /**
  * 스크롤을 허용하고 이전 스크롤 위치 복구
  */
-const allowScroll = (prevScrollY: number) => {
+const unlock = (scrollY: number) => {
   document.body.style.position = '';
   document.body.style.top = '';
   document.body.style.overflowY = '';
 
-  window.scrollTo({ top: prevScrollY, behavior: 'instant' });
+  window.scrollTo({ top: scrollY, behavior: 'instant' });
 };
 
 export function useBodyScrollLock(isLocked: boolean): void {
@@ -33,7 +33,7 @@ export function useBodyScrollLock(isLocked: boolean): void {
 
     if (!hasScroll) return;
 
-    const prevScrollY = preventScroll();
-    return () => allowScroll(prevScrollY);
+    const scrollY = lock();
+    return () => unlock(scrollY);
   }, [isLocked]);
 }

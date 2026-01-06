e4d0cbd1205c4cd69da9f1c5dda5c904d0361675
jinhok96 <jinhok96a@gmail.com>
2026-01-06 18:10:08 +0900

    [25.1.6] 문구 추가

--- apps/web/src/views/home/ui/HomeTechStackSection.tsx
+++ apps/web/src/views/home/ui/HomeTechStackSection.tsx
@@ -8,7 +8,21 @@ type Props = {
 export function HomeTechStackSection({ id, label }: Props) {
   return (
     <HomeSection id={id}>
-      <HomeSection.Header label={label}>기술 스택입니다</HomeSection.Header>
+      <HomeSection.Header label={label}>다양한 기술 및 도구를 사용할 수 있어요</HomeSection.Header>
+
+      <div>
+        프론트엔드: Javascript, Typescript, React, Vue, Next.js, React Native
+        <br />
+        스타일: CSS, SASS, Styled Component, TailwindCSS
+        <br />
+        라이브러리: Tanstack Query, SWR, Zustand, React Hook Form, Jest, Motion
+        <br />
+        번들러, 배포: Webpack, Babel, Electron, Turbopack, Vercel
+        <br />
+        디자인: Figma, Illustrator, Photoshop, InDesign, After Effects
+        <br />
+        협업: Git, Github, Slack, Trello, Jira, Notion
+      </div>
     </HomeSection>
   );
 }

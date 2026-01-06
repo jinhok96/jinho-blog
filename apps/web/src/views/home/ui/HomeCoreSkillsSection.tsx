47bf86c18cef3588a50696644641c115519e3dee
jinhok96 <jinhok96a@gmail.com>
2026-01-06 16:31:39 +0900

    [25.1.6] 주석 추가

--- apps/web/src/views/home/ui/HomeCoreSkillsSection.tsx
+++ apps/web/src/views/home/ui/HomeCoreSkillsSection.tsx
@@ -5,6 +5,12 @@ type Props = {
   label: string;
 };
 
+/**
+ *  1. 빠른 학습과 발전: 상황에 따라 필요한 기술을 찾고 빠르게 습득 후 숙련도 향상
+    2. 높은 UI/UX 이해도: 시각디자인을 전공, 어플리케이션 리디자인 및 웹페이지 디자인 경험
+    3. 효율적인 AI 활용: AI 에이전트를 활용해 빠른 프로젝트 구현부터 MCP를 연결해 최적화 및 문서화한 경험
+ */
+
 export function HomeCoreSkillsSection({ id, label }: Props) {
   return (
     <HomeSection id={id}>

2ae12cda88427c59298661f4b1bb6b1bbd27a155
jinhok96 <jinhok96a@gmail.com>
2026-01-07 13:55:55 +0900

    [25.1.7] 필요없는 코드 제거

--- apps/web/src/core/ui/badge/types.ts
+++ apps/web/src/core/ui/badge/types.ts
@@ -1,10 +1,5 @@
-import type { TechStack } from '@jinho-blog/shared';
 import type { PropsWithChildren } from 'react';
 
 export type BadgeProps = PropsWithChildren<{
   className?: string;
 }>;
-
-export interface TechBadgeProps extends Omit<BadgeProps, 'children'> {
-  tech: TechStack;
-}

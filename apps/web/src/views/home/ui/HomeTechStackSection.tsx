bc77b590e6c3b3a80ea6b0393680c5aed8e133b4
jinhok96 <jinhok96a@gmail.com>
2026-01-07 12:45:40 +0900

    [25.1.7] HomeTechStackSection UI 업데이트

--- apps/web/src/views/home/ui/HomeTechStackSection.tsx
+++ apps/web/src/views/home/ui/HomeTechStackSection.tsx
@@ -1,4 +1,34 @@
+import type { TechStack } from '@jinho-blog/shared';
+
 import { HomeSection } from '@/views/home/ui/HomeSection';
+import { TechStackItem } from '@/views/home/ui/techStackSection/TechStackItem';
+
+type StackProps = {
+  label: string;
+  techs: TechStack[];
+};
+
+function Stack({ label, techs }: StackProps) {
+  return (
+    <div className="size-full">
+      <p className="mb-4 w-full text-left font-caption-14 text-foreground-6">{label}</p>
+
+      <ol
+        className={`
+          grid grid-cols-3 gap-2
+          tablet:gap-2.5
+        `}
+      >
+        {techs.map(tech => (
+          <TechStackItem
+            key={tech}
+            tech={tech}
+          />
+        ))}
+      </ol>
+    </div>
+  );
+}
 
 type Props = {
   id: string;
@@ -10,18 +40,36 @@ export function HomeTechStackSection({ id, label }: Props) {
     <HomeSection id={id}>
       <HomeSection.Header label={label}>여러 기술 및 도구를 사용할 수 있어요</HomeSection.Header>
 
-      <div>
-        프론트엔드: Javascript, Typescript, React, Vue, Next.js, React Native
-        <br />
-        스타일: CSS, SASS, Styled Component, TailwindCSS
-        <br />
-        라이브러리: Tanstack Query, SWR, Zustand, React Hook Form, Jest, Motion
-        <br />
-        번들러, 배포: Webpack, Babel, Electron, Turbopack, Vercel
-        <br />
-        디자인: Figma, Illustrator, Photoshop, InDesign, After Effects
-        <br />
-        협업: Git, Github, Slack, Trello, Jira, Notion
+      <div
+        className={`
+          grid size-full max-w-sm grid-cols-2 gap-x-6 gap-y-7
+          tablet:max-w-2xl tablet:grid-cols-3 tablet:gap-8
+        `}
+      >
+        <Stack
+          label="프론트엔드"
+          techs={['html', 'javascript', 'typescript', 'react', 'vue', 'nextjs', 'react-native']}
+        />
+        <Stack
+          label="스타일"
+          techs={['css', 'sass', 'styled-component', 'tailwindcss']}
+        />
+        <Stack
+          label="라이브러리"
+          techs={['tanstack-query', 'swr', 'zustand', 'react-hook-form', 'jest', 'motion']}
+        />
+        <Stack
+          label="번들러, 배포"
+          techs={['webpack', 'babel', 'electron', 'turbopack', 'vercel']}
+        />
+        <Stack
+          label="디자인"
+          techs={['figma', 'illustrator', 'photoshop', 'in-design', 'after-effects']}
+        />
+        <Stack
+          label="협업"
+          techs={['git', 'github', 'slack', 'trello', 'jira', 'notion']}
+        />
       </div>
     </HomeSection>
   );
---
+++ apps/web/src/views/home/ui/techStackSection/TechStackItem.tsx
@@ -0,0 +1,59 @@
+'use client';
+
+import type { TechStack } from '@jinho-blog/shared';
+
+import { useRef, useState } from 'react';
+
+import { useOutsideClickEffect, useTimeoutRef } from '@/core/hooks';
+import { TECH_STACK_MAP } from '@/core/map';
+import { cn } from '@/core/utils';
+
+const VISIBLE_TIMEOUT = 3000;
+
+type Props = {
+  tech: TechStack;
+};
+
+export function TechStackItem({ tech }: Props) {
+  const [isVisible, setIsVisible] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+  const timeout = useTimeoutRef();
+
+  const handleImageClick = () => {
+    setIsVisible(true);
+    timeout.set(() => setIsVisible(false), VISIBLE_TIMEOUT);
+  };
+
+  useOutsideClickEffect(() => {
+    setIsVisible(false);
+  }, [ref]);
+
+  return (
+    <li className="relative">
+      {/* 아이콘 */}
+      <div
+        className="peer aspect-square w-full overflow-hidden rounded-md bg-foreground-2"
+        onClick={handleImageClick}
+        ref={ref}
+      >
+        Icon
+      </div>
+
+      {/* 라벨 */}
+      <div
+        className={cn(
+          `
+            pointer-events-none absolute bottom-full left-1/2 mb-2 size-fit -translate-x-1/2 rounded-lg bg-foreground-7
+            px-2 py-1 text-center align-text-bottom font-body-14 leading-tight text-background opacity-0
+            backdrop-blur-sm select-none animated-100
+            peer-hover:opacity-100
+            dark:font-medium
+          `,
+          isVisible && 'touch:opacity-100',
+        )}
+      >
+        {TECH_STACK_MAP[tech]}
+      </div>
+    </li>
+  );
+}

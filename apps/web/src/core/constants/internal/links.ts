efe2ea5cda6fcbe2d1c58e512bbb62ef799fd752
jinhok96 <jinhok96a@gmail.com>
2026-01-08 16:39:22 +0900

    [25.1.8] 링크, robots 수정

--- apps/web/src/core/constants/internal/links.ts
+++ apps/web/src/core/constants/internal/links.ts
@@ -1,4 +1,6 @@
 export const LINKS = {
   GITHUB: 'https://github.com/jinhok96',
   JINHO_BLOG_REPOSITORY: 'https://github.com/jinhok96/jinho-blog',
+  RSS: '/rss.xml',
+  SITEMAP: '/sitemap.xml',
 };

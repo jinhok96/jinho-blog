0e898fd79fddbaa99ef9cddcd1d91e8da5ab4e37
jinhok96 <jinhok96a@gmail.com>
2026-01-09 13:04:13 +0900

    [25.1.9] 롤백

--- apps/web/src/views/blog/ui/BlogContentSection.tsx
+++ apps/web/src/views/blog/ui/BlogContentSection.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react-hooks/error-boundaries */
 import type { Blog } from '@jinho-blog/mdx-handler';
 import type { PaginatedResult } from '@jinho-blog/shared';
 
@@ -10,31 +9,26 @@ type Props = {
 };
 
 export async function BlogContentSection({ posts }: Props) {
-  try {
-    return (
-      <div className="w-full">
-        <Show
-          when={posts.length}
-          fallback={ContentCardSection.Placeholder}
-        >
-          <ContentCardSection>
-            {posts.map(({ category, slug, path, title, description, createdAt }) => (
-              <ContentCardSection.Card
-                key={slug}
-                href={path}
-                category={BLOG_CATEGORY_MAP[category]}
-                title={title}
-                createdAt={createdAt}
-              >
-                <ContentCardSection.BlogInfo description={description} />
-              </ContentCardSection.Card>
-            ))}
-          </ContentCardSection>
-        </Show>
-      </div>
-    );
-  } catch (error) {
-    console.error('에러에러에러', error);
-    throw error;
-  }
+  return (
+    <div className="w-full">
+      <Show
+        when={posts.length}
+        fallback={ContentCardSection.Placeholder}
+      >
+        <ContentCardSection>
+          {posts.map(({ category, slug, path, title, description, createdAt }) => (
+            <ContentCardSection.Card
+              key={slug}
+              href={path}
+              category={BLOG_CATEGORY_MAP[category]}
+              title={title}
+              createdAt={createdAt}
+            >
+              <ContentCardSection.BlogInfo description={description} />
+            </ContentCardSection.Card>
+          ))}
+        </ContentCardSection>
+      </Show>
+    </div>
+  );
 }

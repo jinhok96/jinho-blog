6c9f3e14565c3f9bde2bb5b3a3b0767c10bcb8bc
jinhok96 <jinhok96a@gmail.com>
2026-02-06 15:47:26 +0900

    [26.2.6] baseUrl 생성 함수 테스트 적용

--- apps/web/src/core/http/internal/config.ts
+++ apps/web/src/core/http/internal/config.ts
@@ -1,4 +1,26 @@
+const getBaseUrl = () => {
+  // 브라우저 환경
+  if (typeof window !== 'undefined') {
+    return '';
+  }
+
+  // SSR 환경
+  // Vercel 자동 제공 (preview, production)
+  if (process.env.VERCEL_URL) {
+    console.log('process.env.VERCEL_URL', process.env.VERCEL_URL);
+    return `https://${process.env.VERCEL_URL}`;
+  }
+
+  // 로컬 개발
+  if (process.env.NEXT_PUBLIC_BASE_URL) {
+    return process.env.NEXT_PUBLIC_BASE_URL;
+  }
+
+  return '';
+};
+
 export const HTTP_DEFAULT_TIMEOUT = 10 * 1000;
 export const HTTP_DEFAULT_RETRY = 3;
-export const HTTP_DEFAULT_BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || '';
+export const HTTP_DEFAULT_BASE_URL = getBaseUrl();
+// export const HTTP_DEFAULT_BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || '';
 export const HTTP_DEFAULT_REVALIDATE_TIME = 60 * 10; // 10분 (초 단위)
